940e8ca3183428f2dbadf196ab510292c17a5a87
aleguztor <kihacaoficial@gmail.com>
2026-01-13 00:21:15 +0100

    feat: :safety_vest: Validation of Requests

--- MediaMarktProjectApi.Application/Dtos/Product/CreateProductRequest.cs
+++ MediaMarktProjectApi.Application/Dtos/Product/CreateProductRequest.cs
@@ -2,13 +2,14 @@
 
 public record CreateProductRequest
 {
-    [Required]
-    [MaxLength(100)]
+    [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+    [MaxLength(50, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
     public required string Name { get; set; }
 
-    [Range(0.01, 10000000)]
+    [Range(0.01, 10000000, ErrorMessage = "El precio debe estar entre 0.01 y 10.000.000.")]
     public decimal Price { get; set; }
 
+    [MaxLength(250, ErrorMessage = "La descripción no puede superar los 250 caracteres.")]
     public string Description { get; set; } = string.Empty;
 
     public Guid? CategoryId { get; set; }
--- MediaMarktProjectApi.Application/Dtos/Product/UpdateProductRequest.cs
+++ MediaMarktProjectApi.Application/Dtos/Product/UpdateProductRequest.cs
@@ -2,13 +2,17 @@
 
 public record UpdateProductRequest
 {
-    public Guid Id { get; set; }
-    [MaxLength(100)]
+    [Required(ErrorMessage = "El id es obligatorio.")]
+    public required Guid Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+    [MaxLength(50, ErrorMessage = "El nombre no puede superar los 50 caracteres.")]
     public string Name { get; set; } = string.Empty;
 
-    [Range(0.01, 10000000)]
+    [Range(0.01, 10000000, ErrorMessage = "El precio debe estar entre 0.01 y 10.000.000.")]
     public decimal Price { get; set; }
 
+    [MaxLength(250, ErrorMessage = "La descripción no puede superar los 250 caracteres.")]
     public string Description { get; set; } = string.Empty;
 
     public Guid? CategoryId { get; set; }
