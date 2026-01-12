74bf94a332e3fbddb84b22a4a730c699c7a00b08
aleguztor <kihacaoficial@gmail.com>
2026-01-12 23:09:59 +0100

    feat: :white_check_mark: Adding new Tests for Product

--- MediaMarktProjectApi.Tests/Controllers/ProductEndpointsTests.cs
+++ MediaMarktProjectApi.Tests/Controllers/ProductEndpointsTests.cs
@@ -1,6 +1,4 @@
-﻿using MediaMarktProjectApi.Tests.Seed;
-
-namespace MediaMarktProjectApi.Tests.Controllers;
+﻿namespace MediaMarktProjectApi.Tests.Controllers;
 
 public class ProductEndpointsTests : IClassFixture<MediaMarktApiFactory>
 {
@@ -26,14 +24,57 @@ public class ProductEndpointsTests : IClassFixture<MediaMarktApiFactory>
         var products = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
         products.Should().NotBeNull();
     }
+    [Fact]
+    public async Task PutProduct_ShouldReturnOkAndCompareIfExistDataInDB()
+    {
+        // CREAMOS UNO
+        CreateProductRequest createProduct = ProductFaker.CreateProductRequestFaker(null);
+        var postResponse = await _client.PostAsJsonAsync("/api/product", createProduct);
+        postResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+        var createdProduct = await postResponse.Content.ReadFromJsonAsync<Product>();
+
+        // UPDATE PRODUCT
+        UpdateProductRequest newUpdateProduct = ProductFaker.CreateUpdateRequestFaker(createdProduct.Id, null);
+        var response = await _client.PutAsJsonAsync("/api/product", newUpdateProduct);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        //VOLVEMOS A OBTENER POR ID Y COMPROBAMOS
+        var getProduct = await _client.GetAsync($"/api/product/{createdProduct.Id}");
+        getProduct.Should().NotBeNull();
+        var updatedProduct = await getProduct.Content.ReadFromJsonAsync<Product>();
+        updatedProduct.Name.Should().NotBe(createdProduct.Name);
+    }
+    [Fact]
+    public async Task CreateProduct_ShouldReturnOkAndReturnProduct()
+    {
+        CreateProductRequest createProduct = ProductFaker.CreateProductRequestFaker(null);
+        var response = await _client.PostAsJsonAsync("/api/product", createProduct);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var products = await response.Content.ReadFromJsonAsync<ProductDto>();
+        products.Should().NotBeNull();
+    }
 
     [Fact]
-    public async Task CreateProduct_WithInvalidData_ShouldReturnBadRequest()
+    public async Task DeleteProduct_ShouldReturnOKAndCheckNotExist()
     {
-        var invalidProduct = new CreateProductRequest { Name = "", Price=-1 }; 
+        CreateProductRequest createProduct = ProductFaker.CreateProductRequestFaker(null);
+        var postResponse = await _client.PostAsJsonAsync("/api/product", createProduct);
+        if (!postResponse.IsSuccessStatusCode)
+        {
+            // Esto lee el cuerpo del error 400 que envía ASP.NET Core
+            var errorBody = await postResponse.Content.ReadAsStringAsync();
+            // Imprime esto en la consola de salida del test
+            throw new Exception($"DETALLE DEL ERROR: {errorBody}");
+        }
+        var createdProduct = await postResponse.Content.ReadFromJsonAsync<Product>();
 
-        var response = await _client.PostAsJsonAsync("/api/product", invalidProduct);
+        var response = await _client.DeleteAsync($"/api/product/{createdProduct.Id}");
 
-        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        response.StatusCode.Should().Be(HttpStatusCode.OK); 
+
+        var getResponse = await _client.GetAsync($"/api/product/{createdProduct.Id}");
+        getResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
--- MediaMarktProjectApi.Tests/Seed/ProductFaker.cs
+++ MediaMarktProjectApi.Tests/Seed/ProductFaker.cs
@@ -1,15 +1,34 @@
-﻿namespace MediaMarktProjectApi.Tests.Seed
+﻿using Bogus.Extensions;
+
+namespace MediaMarktProjectApi.Tests.Seed;
+public class ProductFaker
 {
-    public class ProductFaker
+    public static Faker<Product> CreateFaker(IEnumerable<Category> categories)
     {
-        public static Faker<Product> CreateFaker(IEnumerable<Category> categories)
-        {
-            return new Faker<Product>()
-                .RuleFor(p => p.Id, f => Guid.NewGuid())
-                .RuleFor(p => p.Name, f => f.Commerce.ProductName())
-                .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
-                .RuleFor(p => p.Price, f => f.Random.Decimal(10, 1000))
-                .RuleFor(p => p.CategoryId, f => f.PickRandom(categories).Id);
-        }
+        return new Faker<Product>()
+            .RuleFor(p => p.Id, f => Guid.NewGuid())
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+            .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(p => p.Price, f => f.Random.Decimal(10, 1000))
+            .RuleFor(p => p.CategoryId, f => f.PickRandom(categories).Id);
     }
+        
+    public static Faker<CreateProductRequest> CreateProductRequestFaker(Category category)
+    {
+        return new Faker<CreateProductRequest>()
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName().ClampLength(max:100))
+            .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(p => p.Price, f => f.Finance.Amount(0.01m, 10000000m, 2))
+            .RuleFor(p => p.CategoryId, f => category?.Id);
+    }
+    public static Faker<UpdateProductRequest> CreateUpdateRequestFaker(Guid id, Category category)
+    {
+        return new Faker<UpdateProductRequest>()
+            .RuleFor(p => p.Id, f => id)
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName().ClampLength(max: 100))
+            .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(p => p.Price, f => f.Finance.Amount(0.01m, 10000000m, 2))
+            .RuleFor(p => p.CategoryId, f => category?.Id);
+    }
+
 }
